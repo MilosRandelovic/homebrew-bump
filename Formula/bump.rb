4860cef01b992d527bfd4891706f6302411d0cc3
MilosRandelovic <MilosRandelovic@users.noreply.github.com>
2025-08-03 12:45:38 +0000

    Update Homebrew formula for release v1.0.1

--- Formula/bump.rb
+++ Formula/bump.rb
@@ -4,8 +4,8 @@
 class Bump < Formula
   desc "A utility to check and update package dependencies"
   homepage "https://github.com/MilosRandelovic/homebrew-bump"
-  url "https://github.com/MilosRandelovic/homebrew-bump/archive/v1.0.0.tar.gz"
-  sha256 "c68fa309ee88982f8a6b64573e762860cbff3f396a7f0c50b1ebb43c20876840"
+  url "https://github.com/MilosRandelovic/homebrew-bump/archive/v1.0.1.tar.gz"
+  sha256 "ac4216839dfe9ec7c22fc86244c77bc7fc0a0d5b6384ecea1dba063926f59dbf"
   license "MIT"
 
   depends_on "go" => :build
